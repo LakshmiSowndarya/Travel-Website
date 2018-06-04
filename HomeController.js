8319a611db87037dee11ddd0381e774c30f2d01a
Sowndarya Kota <KH2167@kitspl.com>
2018-06-04 11:28:27 +0530

    adding changes to test in Home controller

--- HomeController.js
+++ HomeController.js
@@ -2,6 +2,7 @@ var app = angular.module("railway",[]);
 app.controller("homeController", function($scope){
 
     $scope.showSuccess = function() {
+		// making login success variable true
         $scope.isLoginSuccess = true;
     };
 });
